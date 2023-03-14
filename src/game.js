136dd1c5f5440547619e9026faf77930503ee605
IsabelleAnn <isabelle.ann85@gmail.com>
2023-03-14 17:53:10 -0500

    Begin creating Ship and Gameboard classes and methods

--- src/game.js
+++ src/game.js
@@ -16,16 +16,57 @@ const Ship = class {
 
 const Gameboard = class {
   constructor() {
-    this.grid = makeGrid();
-    this.ship1 = new Ship(1);
-    this.ship2 = new Ship(2);
-    this.ship3 = new Ship(3);
-    this.ship4 = new Ship(4);
-    this.ship5 = new Ship(5);
+    this.boardSize = [10, 10];
+    this.misses = [];
+    this.ships = this.#generateShips(this.boardSize);
   }
-  makeGrid() {
-    return { A: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] };
+
+  #generateShips(boardSize) {
+    let maxShips = 5;
+    for (let i = 1; i <= maxShips; i++) {
+      this.ships.i = {
+        details: new Ship(i),
+        coords: [],
+      };
+    }
+    for (const prop in this.ships) {
+      prop.coords = this.#setRandomCoordinates(prop.details.length, boardSize);
+    }
   }
+
+  #setRandomCoordinates(length, boardSize) {
+    let maxRows = boardSize[0];
+    let maxCols = boardSize[1];
+    let rowStart = this.#getRandomNum(1, maxRows);
+    let colStart = this.#getRandomNum(1, maxCols);
+    let coords = [[rowStart, colStart]];
+    let horizontal = this.#getRandomNum(1, 2) == 1 ? true : false;
+
+    for (let i = 2; i <= length; i++) {
+      if (horizontal) {
+        if (length > maxRows - rowStart) {
+          colStart--;
+          coords.shift([rowStart, colStart]);
+        } else {
+          colStart++;
+          coords.push([rowStart, colStart]);
+        }
+      } else {
+        if (length > maxCols - colStart) {
+          rowStart--;
+          coords.shift([rowStart, colStart]);
+        }
+        rowStart++;
+        coords.push([rowStart, colStart]);
+      }
+    }
+    return coords;
+  }
+
+  #getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
   //takes pair of coordinates, checks if hit ship, calls hit on ship or records coordinates of missed
   receiveAttack(x, y) {}
 };
--- src/game.test.js
+++ src/game.test.js
@@ -1,7 +1,13 @@
 import { test, expect } from "jest";
 import * as game from "game.js";
-let ship5 = new Ship(5);
-ship5.hit();
-test("increase number of hits in ship", () => {
-  expect(ship5.hits).toBe(1);
-});
+
+//test if coords length matches length of ship
+// test("increase number of hits in ship", () => {
+//   expect(ship5.hits).toBe(1);
+// });
+
+//test if coords are consecutive for row
+
+//test if coords are consecutive for col
+
+//test if coords stay on grid
