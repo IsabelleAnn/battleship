e476f66aeaa9e5307be6c342d80e15c0f725e121
IsabelleAnn <isabelle.ann85@gmail.com>
2023-03-17 17:42:19 -0500

    Switch from class to factory function format, Rewrite generateShips function

--- src/game.js
+++ src/game.js
@@ -1,58 +1,77 @@
-class Ship {
-  constructor(length) {
-    this.length = length;
-    this.hits = 0;
-    this.sunk = false;
-  }
-  hit() {
-    this.hits++;
-  }
-  isSunk() {
-    if (this.hits === this.length) {
-      this.isSunk = true;
-    }
-  }
-}
+// class Ship {
+//   constructor(length) {
+//     this.length = length;
+//     this.hits = 0;
+//     this.sunk = false;
+//   }
+//   hit() {
+//     this.hits++;
+//   }
+//   isSunk() {
+//     if (this.hits === this.length) {
+//       this.isSunk = true;
+//     }
+//   }
+// }
 
-export class Gameboard {
-  constructor() {
-    this.boardSize = [10, 10];
-    this.misses = [];
-    this.ships = this.generateShips(this.boardSize);
-  }
+const ship = (length) => {
+  let hits = 0;
 
-  generateShips(boardSize) {
-    let maxShips = 5;
-    let ships = {};
-    for (let i = 1; i <= maxShips; i++) {
-      ships[i] = {
-        details: new Ship(i),
-      };
+  const hit = () => {
+    hits++;
+  };
+
+  const isSunk = () => {
+    return hits === length ? true : false;
+  };
+  return { hit, isSunk };
+};
+
+export const gameBoard = () => {
+  const boardSize = [10, 10];
+
+  const generateBoard = () => {
+    //example
+    let board = {
+      1: {
+        1: { status: "none" },
+        2: { status: miss },
+        3: { status: Battleship },
+      },
+    };
+    for (let i = 1; i <= boardSize[0]; i++) {
+      for (let j = 0; j <= boardSize[1]; j++) {
+        if (j === 0) {
+          // board[i].push("");
+        } else {
+          // board[i].push(j);
+        }
+      }
     }
-    for (const ship in this.ships) {
-      ship.coords = this.setRandomCoordinates(ship.details.length, boardSize);
-    }
+  };
+
+  const generateShips = () => {
+    let ships = {
+      Carrier: { details: ship(5), coords: setCoordinates(5) },
+
+      Battleship: { details: ship(4), coords: setCoordinates(4) },
+
+      // Cruiser: { details: ship(3), coords: setCoordinates(3) },
+
+      Submarine: { details: ship(3), coords: setCoordinates(3) },
+
+      Destroyer: { details: ship(2), coords: setCoordinates(2) },
+    };
     return ships;
-  }
+  };
 
-  setCoordinates(length) {
-    let coords = [[length, length]];
-    let increment = length;
-    for (let i = 1; i < length; i++) {
-      increment++;
-      coords.push([length, increment]);
-    }
-
-    return coords;
-  }
-
-  setRandomCoordinates(length, boardSize) {
-    let maxRows = boardSize[0];
-    let maxCols = boardSize[1];
-    let rowStart = this.getRandomNum(1, maxRows);
-    let colStart = this.getRandomNum(1, maxCols);
+  const placeShipRandomly = (length) => {
+    const maxRows = boardSize[0];
+    const maxCols = boardSize[1];
+    let rowStart = getRandomNum(1, maxRows);
+    let colStart = getRandomNum(1, maxCols);
     let coords = [[rowStart, colStart]];
-    let horizontal = this.getRandomNum(1, 2) == 1 ? true : false;
+    let horizontal = getRandomNum(1, 2) == 1 ? true : false;
     //make sure coords aren't repeated!!
     for (let i = 1; i < length; i++) {
       if (horizontal) {
@@ -74,12 +93,103 @@ export class Gameboard {
       }
     }
     return coords;
-  }
+  };
 
-  getRandomNum(min, max) {
+  const setCoordinates = (length) => {
+    let coords = [[length, length]];
+    let increment = length;
+    for (let i = 1; i < length; i++) {
+      increment++;
+      coords.push([length, increment]);
+    }
+    return coords;
+  };
+
+  const getRandomNum = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
-  }
+  };
+  return { generateShips, generateBoard };
+};
 
-  //takes pair of coordinates, checks if hit ship, calls hit on ship or records coordinates of missed
-  receiveAttack(x, y) {}
-}
+// export class Gameboard {
+//   constructor() {
+//     this.boardSize = [10, 10];
+//     this.board = this.generateBoard(this.boardSize);
+//     this.misses = [];
+//     this.ships = this.generateShips();
+//   }
+
+//   generateBoard() {
+//     let board = [[""]];
+//     for (let i = 1; i <= this.boardSize[0]; i++) {
+//       for (let j = 0; j <= this.boardSize[1]; j++) {
+//         if (j === 0) {
+//           board[i].push("");
+//         } else {
+//           board[i].push(j);
+//         }
+//       }
+//     }
+//   }
+
+//   generateShips() {
+//     let maxShips = 5;
+//     let ships = {};
+//     for (let i = 1; i <= maxShips; i++) {
+//       ships[i] = {
+//         details: new Ship(i),
+//       };
+//     }
+//     for (const ship in this.ships) {
+//       ship.coords = this.setRandomCoordinates(ship.details.length);
+//     }
+//     return ships;
+//   }
+
+//   setCoordinates(length) {
+//     let coords = [[length, length]];
+//     let increment = length;
+//     for (let i = 1; i < length; i++) {
+//       increment++;
+//       coords.push([length, increment]);
+//     }
+//     return coords;
+//   }
+
+//   setRandomCoordinates(length) {
+//     let maxRows = this.boardSize[0];
+//     let maxCols = this.boardSize[1];
+//     let rowStart = this.getRandomNum(1, maxRows);
+//     let colStart = this.getRandomNum(1, maxCols);
+//     let coords = [[rowStart, colStart]];
+//     let horizontal = this.getRandomNum(1, 2) == 1 ? true : false;
+//     //make sure coords aren't repeated!!
+//     for (let i = 1; i < length; i++) {
+//       if (horizontal) {
+//         if (length > maxRows - rowStart) {
+//           colStart--;
+//           coords.unshift([rowStart, colStart]);
+//         } else {
+//           colStart++;
+//           coords.push([rowStart, colStart]);
+//         }
+//       } else {
+//         if (length > maxCols - colStart) {
+//           rowStart--;
+//           coords.unshift([rowStart, colStart]);
+//         } else {
+//           rowStart++;
+//           coords.push([rowStart, colStart]);
+//         }
+//       }
+//     }
+//     return coords;
+//   }
+
+//   getRandomNum(min, max) {
+//     return Math.floor(Math.random() * (max - min) + min);
+//   }
+
+//   //takes pair of coordinates, checks if hit ship, calls hit on ship or records coordinates of missed
+//   receiveAttack(x, y) {}
+// }
--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -1,47 +1,33 @@
 import { test, expect } from "@jest/globals";
-import { Gameboard } from "../game.js";
+import { gameBoard } from "../game.js";
 
-let board = new Gameboard();
-test("boardsize is [10, 10]", () => {
-  expect(board.boardSize).toEqual([10, 10]);
-});
-
-test("length of ship 2 in ships object is 2", () => {
-  expect(board.ships[2].details.length).toEqual(2);
-});
-
-test("setrandomcoordinates(1) returns array with length of 1", () => {
-  expect(board.setRandomCoordinates(1, [10, 10]).length).toEqual(1);
-});
-
-test("setcoords(1) returns array with length of 1", () => {
-  expect(board.setCoordinates(1).length).toEqual(1);
-});
-test("setcoords(2) returns array equal to [2,2][2,3]", () => {
-  expect(board.setCoordinates(2)).toEqual([
+let newGame = gameBoard();
+let ships = newGame.generateShips();
+test("coords of Destroyer should be [[2,2],[2,3]]", () => {
+  expect(ships.Destroyer.coords).toEqual([
     [2, 2],
     [2, 3],
   ]);
 });
-
-test("setrandomcoordinates(5) returns array with length of 5", () => {
-  expect(board.setRandomCoordinates(5, [10, 10]).length).toEqual(5);
+test("coords of Submarine should be [[3,3],[3,4],[3,5]]", () => {
+  expect(ships.Submarine.coords).toEqual([
+    [3, 3],
+    [3, 4],
+    [3, 5],
+  ]);
 });
-// test("setrandomcoordinates(2) returns array equal to [2,2][2,3]", () => {
-//   expect(board.setrandomcoordinates(2)).toEqual([
-//     [2, 2],
-//     [2, 3],
-//   ]);
-// });
-// test("", () => {
-//    let ships = board.generateShips([10, 10]);
-//   expect(board.generateShips([10, 10]).ship[1].coords.length).toEqual(1);
-// });
+test("coords of Carrier should be [[5,5],[5,6],[5,7],[5,8],[5,9]]", () => {
+  expect(ships.Carrier.coords).toEqual([
+    [5, 5],
+    [5, 6],
+    [5, 7],
+    [5, 8],
+    [5, 9],
+  ]);
+});
 
-// test("length of coords of ship 1 in ships object is 1", () => {
-//   expect(board.ships[1].coords.length).toEqual(1);
-// });
+test("board", () => {
+  expect().toEqual();
+});
 
-//test if coords are consecutive for col
-
-//test if coords stay on grid
+// test("", () => {});
